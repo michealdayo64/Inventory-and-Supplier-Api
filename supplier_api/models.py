bd4899bec6a0a0cd7ca8398a69c31a154973ee9b
michealdayo64 <omotoshomicheal93@gmail.com>
2024-06-16 08:07:16 +0100

    ok

--- supplier_api/models.py
+++ supplier_api/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 
 # Supplier Record Table
 
 class SupplierRecord(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
-    phone_number = models.IntegerField(validators=[MinValueValidator(0),
-                                       MaxValueValidator(12)], blank=True, null=True)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, null=True)
     address = models.TextField(null=True, blank=True)
     date_created = models.DateTimeField(
         auto_now_add=True, null=True, blank=True)
