1f39bf9708a37e832813141811c58d259b9ac6fb
michealdayo64 <omotoshomicheal93@gmail.com>
2024-06-16 07:42:17 +0100

    ok

--- inventory_api/views.py
+++ inventory_api/views.py
@@ -11,7 +11,7 @@ from inventory_api.serializers import ItemSerializer, UserSerializer
 # Create your views here.
 
 
-# API VIEW TO CREATE EMPLOYEE ACCOUNT
+# API VIEW TO CREATE EMPLOYEE ACCOUNT...
 
 
 class UserCreate(APIView):
